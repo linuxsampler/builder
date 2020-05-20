a80f5e7df10d7996411d44b284def396e2c29598
Shamil Yakupov <syakupov@ugcs.com>
2020-05-20 03:22:42 +0300

    Fix DEB package

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ sudo docker rm -f $DOCKER_IMAGE || true
 sudo docker build --build-arg LINUXSAMPLER_VERSION="$LINUXSAMPLER_VERSION" --build-arg UBUNTU="ubuntu:$UBUNTU_VERSION" . -t linuxsampler-$LINUXSAMPLER_VERSION-ubuntu-$UBUNTU_VERSION
 
 sudo docker run -it -d --name $DOCKER_IMAGE $DOCKER_IMAGE /bin/bash
-sudo docker exec -it $DOCKER_IMAGE make -j8
-sudo docker exec -it $DOCKER_IMAGE checkinstall -D -y --nodoc --backup=no --pkglicense="GPL2" --maintainer="S. Yakupov \\<s.yakupov@noviga.com\\>" --requires="libasound2,libgig9,libjack0,libsndfile1,libsqlite3-0" --install=no
+sudo docker exec -it $DOCKER_IMAGE make -j$(nproc)
+sudo docker exec -it $DOCKER_IMAGE checkinstall -D -y --nodoc --backup=no --pkglicense="GPL2" --maintainer="S. Yakupov \\<s.yakupov@noviga.com\\>" --requires="libasound2,libgig9,jackd,libsndfile1,libsqlite3-0" --install=no
 sudo docker cp $DOCKER_IMAGE:/root/linuxsampler-$LINUXSAMPLER_VERSION/linuxsampler_$LINUXSAMPLER_VERSION-1_amd64.deb ./
 sudo chown $USER linuxsampler_$LINUXSAMPLER_VERSION-1_amd64.deb
